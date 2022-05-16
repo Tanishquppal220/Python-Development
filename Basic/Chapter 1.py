520ac14b7bdc5ba263cd511eed1d69f562c16368
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-16 20:39:51 +0530

    Chapter 1

---
+++ Basic/Chapter 1.py
@@ -0,0 +1,6 @@
+'''import os
+print("Hello world")'''
+print("Hello I am Tanishq")
+print('''Hello
+tanishq
+here''')
---
+++ Basic/PlayingMusic.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\Tanishq\\Desktop\\School work\\Tanishq\\Development\\Python Development\\Basic\\play.mp3')
