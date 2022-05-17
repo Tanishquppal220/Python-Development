465dabe6f078d88dbc089b9fc64e043178ed89d0
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-17 18:45:54 +0530

    Basic

--- Basic/PlayingMusic.py
+++ Basic/PlayingMusic.py
@@ -1,2 +1,2 @@
 from playsound import playsound
-playsound('C:\\Users\\Tanishq\\Desktop\\School work\\Tanishq\\Development\\Python Development\\Basic\\play.mp3')
+playsound('C:\\Users\\Tanishq\\Desktop\\School-work\\Tanishq\\Development\\Python-Development\\Basic\\play.mp3')
