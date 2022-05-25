3a7d6305cfad010167b2cc756d13d57e44a04be1
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-25 13:11:35 +0530

    dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+d1 = ()
+print(type(d1))
+
+# dictionary
+d2 = {"Tanishq": "11",
+      "Manik": "10",
+      "Hardik": "6",
+      "school": {"Apeejay", "mgn", }}
+d2["Greet"] = "Good Moring"
+print(d2)
+del d2["school"]
+print(d2)
