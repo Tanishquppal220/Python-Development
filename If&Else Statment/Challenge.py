a9a21a02b264d77d96561d3e0254144945c7d8b8
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-26 18:08:54 +0530

    If&Else Statment

---
+++ If&Else Statment/Challenge.py
@@ -0,0 +1,6 @@
+age = int(input("Enter Your Age: \n"))
+
+if age>=18:
+    print("You are authorized to access this account. You have a great day!")
+else:
+    print("You are not yet authorized to access this account")
---
+++ If&Else Statment/If&Else Statment.py
@@ -0,0 +1,25 @@
+# a = 3
+# b = 45
+
+# var1 = int(input())
+
+# if var1 > (a+b):
+#     print("ok")
+# elif var1 == (a+b):
+#     print("not ok")
+# else:
+#     print("error")
+
+list=[5,45,7,87]
+# list.remove(7)
+list.remove(45)
+if 45 and 7 in list:
+    print("Both numbers")
+elif 7 in list:
+    print("Only 7")
+elif  45 in list:
+    print("Only 45")
+else:
+    print("No Number")
+
+
