fb633fe37637740ca589a7c39ed2b2c5aa839b7d
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-26 16:47:11 +0530

    exersice 1

---
+++ dictionary/practice.py
@@ -0,0 +1,6 @@
+dict1= {"Mutable":"Can Be Changed",
+        "Immutable":"Cannot Be Changed",
+        "Set":"An Array of Objects"}
+key =input("Enter Your Word:\n")
+print("Meaning of",key,"is","\'",dict1[key],"\'")
+
