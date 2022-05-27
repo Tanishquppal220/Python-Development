6bcb6cbcf2c66948c71ba491b316134a55718422
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-27 18:33:28 +0530

    calculator

--- Exercise/calculator.py
+++ Exercise/calculator.py
@@ -1,9 +1,22 @@
-print("Enter First Number:")
-num1 = int(input())
-print("Enter Second Number:")
-num2 = int(input())
+a = int(input("Enter First Number:\n"))
+b = int(input("Enter Second Number:\n"))
+o = (input("Enter Oprator:\n"))
+if a==45 and b==3 and o=="*":
+    print("Your Answer is \n"+str(555))
+elif a==3 and b==45 and o=="*":
+    print("Your Answer is \n"+str(555))
+elif a==56 and b==9 and o=="+": 
+    print("Your Answer is \n"+str(77))
+elif a==9 and b==56 and o=="+": 
+    print("Your Answer is \n"+str(77))
+elif a==56 and b==6 and o=="/": 
+    print("Your Answer is \n"+str(4))    
+elif o=="+": 
+    print("Your Answer is:\n"+str(a+b))
+elif o=="-": 
+    print("Your Answer is:\n"+str(a-b))
+elif o=="*": 
+    print("Your Answer is:\n"+str(a*b))
+elif o=="/": 
+    print("Your Answer is:\n"+str(a/b))
 
-
-sum = str(num1 + num2)
-
-print("Your result is: \n" + sum)
