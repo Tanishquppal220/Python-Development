fbbd08992fb9b50b70fa5aa6b16aeba553601cd2
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-30 13:35:54 +0530

    Oprators

---
+++ Oprators/Oprators.py
@@ -0,0 +1,42 @@
+# Arithmetic operator
+'''
+print(90//4)
+print(90/4)
+print(25**2)
+print(25%7)
+'''
+
+# Assignment oparators
+'''
+x= 4
+print("Value of x is:",x)
+x+=10
+print("Value of x is:",x)
+'''
+
+# Comparison Operators
+'''
+x=6
+print(x or 7)
+'''
+
+# Logical Oprators
+'''
+a=True
+b=False
+print(a or b)
+'''
+
+# Identity Operator
+'''
+a=True
+b=False
+print(a is b)
+print(a is not b)
+'''
+
+# Membership Operators
+'''
+list = [3,4,66,52,3435,46,5,799,8]
+print(3435 not in  list)
+'''
