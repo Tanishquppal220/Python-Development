38022ac86a888f0ef54c509e565279077c2effda
Tanishq220 <Tanishquppal220@gmail.com>
2022-09-04 15:44:02 +0530

    did it

---
+++ Practical/practical7.py
@@ -0,0 +1,7 @@
+a = (15, 76, 34, 79, 89, 33, 14, 10, 65, 55)
+print("a Items = ", a)
+a = sorted(a)
+print("Largest Item in a  = ", a[len(a) - 1])
+print("Smallest Item in a  = ", a[0])
+
+    
--- Practical/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-i=3
-while i<=n: 
-    c=a+b 
-    print(c,end=" ") 
-    a=b 
-    b=c 
-    i=i+1 
