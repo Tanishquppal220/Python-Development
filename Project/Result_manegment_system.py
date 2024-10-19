5cdf4bc35186020bde81760fd5a3f01b39fb6267
Tanishq Uppal <tanishquppal220@gmail.com>
2024-10-19 19:50:25 +0000

    Complete result_mangement_system

--- Project/Result_manegment_system.py
+++ Project/Result_manegment_system.py
@@ -5,10 +5,9 @@ import sqlite3
 # check if database.db is present or not
 if not os.path.exists("Project/database.db"):
     # create database.db
-    open("database.db", "w").close()
-
-# connet to database.db
-conn = sqlite3.connect("database.db")
+    os.system("touch Project/database.db")
+# connect to database.db
+conn = sqlite3.connect("Project/database.db")
 # create cursor object
 cur = conn.cursor()
 # create table if table of name student is not present
@@ -27,6 +26,9 @@ def fun():
     n = eval(input("Enter Number Of students To be Added: "))
     for k in range(n):
         r = int(input("Enter Roll Number Of Student: "))
+        if list(cur.execute(f"SELECT * FROM student WHERE roll_no={r}")):
+            print("Roll Number Already Exist")
+            continue
         name = input("Enter Name Of Students: ")
         maths = round(float(input("Enter Marks Of Maths: ")))
         physics = round(float(input("Enter Marks Of Physics: ")))
@@ -35,71 +37,83 @@ def fun():
         computer = round(float(input("Enter Marks Of Computer: ")))
         total = maths + physics + chemistry + english + computer
         percentage = round((total / 500) * 100)
-        # STORE DATA INTO DATABASE
         cur.execute(
             f"INSERT INTO student VALUES({r}, '{name}', {maths}, {physics}, {chemistry}, {english}, {computer}, {total}, {percentage})")
-
+        conn.commit()  # Commit the transaction
 
 def display(x):
     """
     This Function Will Take Roll Number As Input And Display The Details Of Student
     """
-    k = cur.execute(f"SELECT * FROM student WHERE roll_no={x}")
-    if k:
-        print(json.dumps(k, indent=4), end="\n\n")
-    else:
-        print("Student Not Found")
-    return 0
+    k = list(cur.execute(f"SELECT * FROM student WHERE roll_no={x}"))
+    if not k:
+        print("No Record Found!")
+        return 0
+    print(json.dumps(k, indent=4), end="\n\n")
 
 
 #  initialize index to 0
-index = 0
-# While loop for menu
-while True:
+def main():
+    """
+    This Function Will Display The Menu And Call The Function According To User's Choice
+    """
+    # make a list of all the roll numbers
+    roll = list(cur.execute("SELECT roll_no FROM student"))
     print(f'\n{"Welcome To Student Management System":^111}')
     print("=" * 111)
     print("1. Add Student Details")
     print("2. Display Student Details ")
     print("3. Student with More Than 75% Marks")
-    print("4.Delete Student Details")
-    print("5. Delete whole dictionary")
-    print("6. Print Whole dict")
+    print("4. Delete Student Details")
+    print("5. Delete All Records")
+    print("6. Show Records Of All Students") 
     print("7. Exit")
-    index = int(input("Enter Your Choice: "))
+    index = int(input("\nEnter Your Choice: "))
+    print()
     if index == 1:
         """
         This calls function fun() to take input from user and store it into database
         """
         fun()
     elif index == 2:
+        """
+        1. Ask user to enter roll number of student
+        2.calls function display() to display the details of student
+        """
         c = int(input("Enter Roll Number Of Student: "))
         display(c)
     elif index == 3:
-        Promoted = []
-        # for i in d:
-        #     if d[i]['Marks']['Percentage'] >= 75:
-        #         Promoted.append(d[i]['Name'])
-        for i in l:
-            if i["Percentage"] >= 75:
-                Promoted.append(i["Name"])
+        """
+        This gives the list of students who have more than 75% marks
+        """
+        Promoted = list(cur.execute("SELECT name FROM student WHERE percentage>75"))
         print(json.dumps(Promoted, indent=4), end="\n\n")
     elif index == 4:
+        """
+        1. Ask user to enter roll number of student
+        2. Ask user to confirm the deletion
+        3. if user confirms then delete the student details
+        """
         c = int(input("Enter Roll Number Of Student: "))
-        confirm = input("Are You Sure You Want To Delete This Student Details (Y/N): ")
         if confirm == "Y":
-            d.pop(c)
-            print("Student Details Deleted Successfully")
-        else:
-            continue
+            confirm = input(f"Are You Sure You Want To Delete Record of Roll_no {c} (Y/N): ").upper()
+            try:
+                cur.execute(f"DELETE FROM student WHERE roll_no={c}")
+                conn.commit()
+                print("Student Record Deleted Successfully")
+            except:
+                print("No Record Found!")
     elif index == 5:
-        confirm = input("Are You Sure You Want To Delete Whole Dictionary (Y/N): ")
+        confirm = input("Are You Sure You Want To Delete Record of every studnet (Y/N): ").upper()
         if confirm == "Y":
-            d.clear()
-            print("Dictionary Deleted Successfully")
-        else:
-            continue
+            cur.execute("DELETE FROM student")
+            conn.commit()
+            print("Records Deleted Successfully")
     elif index == 6:
-        print(json.dumps(d, indent=4), end="\n\n")
+        all_data = list(cur.execute("SELECT * FROM student"))
+        print(json.dumps(all_data, indent=4), end="\n\n")
     else:
         print("Thank You")
-        break
+        return 0
+    return main()
+main()
